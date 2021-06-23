d599b878842ffc5d284386f10d996a28127c2dcc
JoeHossam <86380699+JoeHossam@users.noreply.github.com>
2021-06-23 22:52:55 +0200

    Create Register.aspx.cs

---
+++ Prjoect/Register.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace MisPrjoect3
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        OleDbConnection connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["database"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            OleDbCommand query = new OleDbCommand(
+                "insert into [user] " +
+                "([uName],[pass],[mail]) " +
+                "values ('" + name.Text + "','" + TextBox2.Text + "','" + TextBox1.Text + "')"
+                , connection);
+
+            OleDbCommand query2 = new OleDbCommand("SELECT @@IDENTITY", connection);
+
+            connection.Open();
+            query.ExecuteNonQuery();
+            Session["userID"] = query2.ExecuteScalar().ToString();
+            Response.Redirect("Home.aspx");
+
+            connection.Close();
+        }
+    }
+}
