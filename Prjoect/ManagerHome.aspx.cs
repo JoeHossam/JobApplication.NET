eea4e70d0b0d3abb7b22bf0954513027e21fe4d6
JoeHossam <86380699+JoeHossam@users.noreply.github.com>
2021-06-23 22:53:52 +0200

    Create ManagerHome.aspx.cs

---
+++ Prjoect/ManagerHome.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MisPrjoect3
+{
+    public partial class Manager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Manager"] != null)
+            {
+                profileHyperLink.Visible = false;
+                logoutHyperLink.Visible = true;
+                registerHyperLink.Visible = false;
+                loginHyperLink.Visible = false;
+                postJobHyperLink.Visible = false;
+                homeHyperLink.NavigateUrl = "ManagerHome.aspx";
+            }
+            else
+            {
+                Response.Redirect("ManagerLogin.aspx");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ManagerJobs.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ManagerUser.aspx");
+        }
+    }
+}
